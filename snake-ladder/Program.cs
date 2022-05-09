d4b65fbb0743172000da9a604e78d51c5e5cacff
suraj807 <surajdev805@gmail.com>
2022-05-09 08:08:35 +0530

    player die to get a number between 1 to 6

--- snake-ladder/Program.cs
+++ snake-ladder/Program.cs
@@ -10,11 +10,17 @@ namespace snake_ladder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Snake Ladder Game");
-            Console.WriteLine("Let's Play");
-            Console.WriteLine("Currently you are at 0 Position");
+            Console.WriteLine("Welcome to Snake Ladder Game\n");
+           
+            int dicenum;   //var
+
+            Random random = new Random();    // Creating object of Random class
+            dicenum = random.Next(1, 7);    // Generating random number 0 , 1 , 2 ,3 ,4,5,6
+            Console.WriteLine($"After rolling Dice,The number display on the Dice is : {dicenum}");
 
             Console.ReadLine();
+
+            
         }
     }
 }
