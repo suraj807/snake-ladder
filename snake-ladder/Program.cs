9a45d9fe73cbdf5049f5db08874fdc845c49e740
suraj807 <surajdev805@gmail.com>
2022-05-09 07:55:15 +0530

    welcome

---
+++ snake-ladder/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake_ladder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to snake and ledder");
+        }
+    }
+}
